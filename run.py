393e34106a49f390890814cc2fbda7085948eeca
Chapman Tse <tch222000@hotmail.com>
2019-05-26 19:48:05 -0700

    Fisher-Yates Shuffle

--- run.py
+++ run.py
@@ -2,8 +2,10 @@ import sys
 
 if sys.version_info[0] < 3:
     execfile("Cards.py")
+    execfile("CardStack.py")
 else:
     exec (open("Cards.py").read())
+    exec (open("CardStack.py").read())
 
 if __name__ == "__main__":
-    analyzeCards(READING_CARDS, GRABBING_CARDS)
+    pass
