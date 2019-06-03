0ce0ea7ce0a3bbe4839d4b3861ce517bb95ee300
Hon Leung Lee <hllee@uw.edu>
2019-06-02 18:02:06 -0700

    Init card stack

--- CardStack.py
+++ CardStack.py
@@ -1,9 +1,16 @@
 import random
 import time
 
-# Fisher-Yates Shuffle
-def cardShuffle(cards):
-    random.seed(time.time())
-    for i in range(len(cards) - 1, 0, -1):
-        j = random.randint(0, i)
-        cards[i], cards[j] = cards[j], cards[i]
+class CardStack:
+    def __init__(self, cards):
+        # top card has index 0
+        self.cards = [card for card in cards]  # not modify the input "cards"
+
+    def shuffle(self):  # Fisher-Yates Shuffle
+        random.seed(time.time())
+        for i in range(len(self.cards) - 1, 0, -1):
+            j = random.randint(0, i)
+            self.cards[i], self.cards[j] = self.cards[j], self.cards[i]
+
+    def draw(self, numCards):
+        return [self.cards.pop(0) for i in range(numCards)]
