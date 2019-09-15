a0ce10448fc2c55a42345c34dcb378c3aecf1386
Hon Leung Lee <hllee@uw.edu>
2019-09-15 15:53:39 -0700

    First commit for Carta Japanese Version support

--- run.py
+++ run.py
@@ -2,12 +2,26 @@ import sys
 import Carta
 
 if __name__ == "__main__":
+    argc = len(sys.argv)
+
+    # python run.py          ---- call EN Carta without debug mode
+    # python run.py debug    ---- call EN Carta with debug mode
+    # python run.py jp       ---- call JP Carta without debug mode
+    # python run.py debug jp ---- call JP Carta with debug mode
     debugMode = False
-    if (len(sys.argv) > 1):
+    inJapanese = False
+
+    if (argc > 1):
         if (sys.argv[1].lower() == "debug"):
             debugMode = True
+        elif (sys.argv[1].lower() == "jp"):
+            inJapanese = True
+
+    if (argc > 2):
+        if (sys.argv[2].lower() == "jp"):
+            inJapanese = True
 
     # First Carta means Carta.py, second Carta means class name Carta
-    c = Carta.Carta(debugMode)
+    c = Carta.Carta(debugMode, inJapanese)
     c.process()
     c.quit()
